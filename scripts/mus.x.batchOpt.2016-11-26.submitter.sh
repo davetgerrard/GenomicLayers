314796a1030ffc4e5e9833c624731579310ca64c
Dave Gerrard <davetgerrard@gmail.com>
2016-11-26 11:30:11 +0000

    batchOpt using min score of 16

---
+++ scripts/mus.x.batchOpt.2016-11-26.submitter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRATCH=/mnt/fls01-home01/mqbssdgb/scratch
+
+SCRIPT_TO_RUN=$SCRATCH/predictfromsequence/scripts/mus.x-inactivationModel.BatchOpt.parallel.fromConfig.R
+
+CONFIG_FILE=$SCRATCH/predictfromsequence/scripts/CONFIG.mus.x-inactivationModel.BatchOpt.2016-11-25.dpsf.R
+
+# The following line specifies that there are 100 jobs
+#$ -t 1-100
+
+#$ -pe smp.pe 16     
+
+# each job to use 16 cores !
+
+# Each time a job is submitted the environment variable SGE_TASK_ID is incremented so to get the nth line of
+
+# The command line to run
+Rscript $SCRIPT_TO_RUN --config $CONFIG_FILE --job_id $SGE_TASK_ID
+
+
+# END OF SCRIPT
