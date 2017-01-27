1c0e14d6581cf097219efab39d6bc0381583aa0b
Dave Gerrard <davetgerrard@gmail.com>
2017-01-27 22:44:03 +0000

    more comments and @export

--- R/createBindingFactor.DNA_motif.R
+++ R/createBindingFactor.DNA_motif.R
@@ -1,8 +1,36 @@
+
+#' Find matches for a binding factor on a layer set
+#'
+#' Generate a list of matches for a binding factor against a layerSet object. 
+#'
+#' @param name method to do something to (\code{"hsv"} or \code{"cluster"})
+#' @param type description of that param
+#' @param patternString  =10 you get the idea
+#' @param patternLength    =10000000 you get the idea
+#' @param stateWidth you get the idea
+#' @param profile.layers you get the idea
+#' @param profile.marks you get the idea
+#' @param mod.layers you get the idea
+#' @param mod.marks you get the idea
+#' @param test.layer0.binding you get the idea
+#' @param test.mismatch.rate you get the idea
+#' @param max.pattern.tries you get the idea
+#' @param min.DM.length you get the idea
+#' @param min.DR.lengt you get the idea
+#' @param verbose you get the idea
+#'
+#' @return \code{"hits"}
+#'
+#' @examples
+#' x <- 1   # great!
+#'
+#' @export
 createBindingFactor.DNA_motif <- function(name,  type="DNA_motif", patternString="N",
                                           patternLength = nchar(patternString), stateWidth=patternLength,
                                           profile.layers="LAYER.1",profile.marks=0,
                                           mod.layers="LAYER.1",mod.marks=1,
-                                      test.layer0.binding=FALSE, test.mismatch.rate=.1 , max.pattern.tries=1000, min.DM.length=2, min.DR.length=10, verbose=FALSE) {
+                                      test.layer0.binding=FALSE, test.mismatch.rate=.1 , max.pattern.tries=1000, 
+                                      min.DM.length=2, min.DR.length=10, verbose=FALSE) {
   
   
   profileList <- list(LAYER.0=list(pattern=DNAString(patternString) , mismatch.rate=0, length=patternLength))
--- R/createBindingFactor.layer_region.R
+++ R/createBindingFactor.layer_region.R
@@ -1,4 +1,29 @@
-
+#' Find matches for a binding factor on a layer set
+#'
+#' Generate a list of matches for a binding factor against a layerSet object. 
+#'
+#' @param name method to do something to (\code{"hsv"} or \code{"cluster"})
+#' @param type description of that param
+#' @param patternString  =10 you get the idea
+#' @param patternLength    =10000000 you get the idea
+#' @param stateWidth you get the idea
+#' @param profile.layers you get the idea
+#' @param profile.marks you get the idea
+#' @param mod.layers you get the idea
+#' @param mod.marks you get the idea
+#' @param test.layer0.binding you get the idea
+#' @param test.mismatch.rate you get the idea
+#' @param max.pattern.tries you get the idea
+#' @param min.DM.length you get the idea
+#' @param min.DR.lengt you get the idea
+#' @param verbose you get the idea
+#'
+#' @return \code{"hits"}
+#'
+#' @examples
+#' x <- 1   # great!
+#'
+#' @export
 createBindingFactor.layer_region <- function(name,  type="layer_region", patternLength=1, patternString="N",
                                              mismatch.rate=0, stateWidth=patternLength,
                                              profile.layers="LAYER.1",  profile.marks=0,
