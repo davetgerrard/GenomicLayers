1761b9c05105edd1164a4a72e0c12f897ae8eb12
Dave Gerrard <davetgerrard@gmail.com>
2017-02-10 23:51:08 +0000

    editing data docs

--- R/data.R
+++ R/data.R
@@ -1,10 +1,14 @@
 #' Mean scores from mm9 chrX
 #'
-#' Mean scores from mm9 chrX
+#' Mean scores from mm9 chrX inactivation
 #' GSM1182890_d3.xist.mus.meanScores
 #'
+#' @docType data
+#' @keywords datasets
+#' @name GSM1182890_d3.xist.mus.meanScores
+#' @usage data(GSM1182890_d3.xist.mus.meanScores)
 #' @format A vector of Mean scores from mm9 chrX
 #'
 #' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3904790/}
-"GSM1182890_d3.xist.mus.meanScores"
+NULL
 
